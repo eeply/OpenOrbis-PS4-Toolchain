4e6d82c2a9635b093050edc0c59d9239c20e4dc1
eeply <quakefree@gmail.com>
2020-09-27 22:09:36 +0200

    Defined some functions of the camera.

--- include/orbis/Camera.h
+++ include/orbis/Camera.h
@@ -7,6 +7,53 @@
 extern "C" {
 #endif
 
+typedef struct OrbisCameraStartParameter {
+  uint32_t size;
+  uint32_t format[2];
+  void* extensions;
+} OrbisCameraStartParameter;
+
+typedef struct OrbisCameraPosition {
+  uint32_t x;
+  uint32_t y;
+  uint32_t width;
+  uint32_t height;
+} OrbisCameraPosition;
+
+typedef struct OrbisCameraFrameData {
+  uint32_t size;
+  uint32_t mode; // 0 is far 1 is near
+  OrbisCameraPosition cameraPosition[8];
+  void* framePointerList[8];
+  uint32_t frameSize[8];
+  uint32_t status[2];
+  uint32_t unkn[70];
+  void* framePointerGarlic[8];
+} OrbisCameraFrameData;
+
+typedef struct OrbisCameraConfigExtention
+{
+	uint32_t format[4];
+	uint32_t resolution;
+	uint32_t framerate;
+	uint32_t width;
+	uint32_t height;
+	uint32_t unknown1;
+	void* unknown2;
+} OrbisCameraConfigExtention;
+
+typedef struct OrbisCameraConfig {
+  uint32_t size;
+  int32_t type;
+  OrbisCameraConfigExtention config[2];
+} OrbisCameraConfig;
+
+typedef struct OrbisCameraVideoSyncParameter {
+  uint32_t size;
+  int32_t syncMode;
+  void* unknown;
+} OrbisCameraVideoSyncParameter;
+
 // Empty Comment
 void sceCameraAudioGetData();
 // Empty Comment
@@ -39,8 +86,8 @@ void sceCameraGetDeviceConfig();
 void sceCameraGetDeviceInfo();
 // Empty Comment
 void sceCameraGetExposureGain();
-// Empty Comment
-void sceCameraGetFrameData();
+// Get camera frame
+int sceCameraGetFrameData(int32_t handle, OrbisCameraFrameData *data);
 // Empty Comment
 void sceCameraGetGamma();
 // Empty Comment
@@ -53,12 +100,12 @@ void sceCameraGetSaturation();
 void sceCameraGetSharpness();
 // Empty Comment
 void sceCameraGetWhiteBalance();
-// Empty Comment
-void sceCameraIsAttached();
+// Check if camera is connected
+int sceCameraIsAttached(int32_t index);
 // Empty Comment
 void sceCameraIsValidFrameData();
-// Empty Comment
-void sceCameraOpen();
+// Open handle with camera
+int sceCameraOpen(int32_t userId, int32_t type, int32_t index, void* params);
 // Empty Comment
 void sceCameraOpenByModuleId();
 // Empty Comment
@@ -75,8 +122,8 @@ void sceCameraSetAutoExposureGain();
 void sceCameraSetAutoWhiteBalance();
 // Empty Comment
 void sceCameraSetCalibData();
-// Empty Comment
-void sceCameraSetConfig();
+// Apply config to camera
+int32_t sceCameraSetConfig(int32_t handle, OrbisCameraConfig* config);
 // Empty Comment
 void sceCameraSetConfigInternal();
 // Empty Comment
@@ -105,14 +152,14 @@ void sceCameraSetProcessFocusByHandle();
 void sceCameraSetSaturation();
 // Empty Comment
 void sceCameraSetSharpness();
-// Empty Comment
-void sceCameraSetVideoSync();
+// Set camera in sync with video output
+int32_t sceCameraSetVideoSync(int32_t handle, OrbisCameraVideoSyncParameter* params);
 // Empty Comment
 void sceCameraSetVideoSyncInternal();
 // Empty Comment
 void sceCameraSetWhiteBalance();
-// Empty Comment
-void sceCameraStart();
+// Start the camera
+int32_t sceCameraStart(int32_t handle, OrbisCameraStartParameter* params);
 // Empty Comment
 void sceCameraStartByHandle();
 // Empty Comment
